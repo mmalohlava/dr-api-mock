43c3c71730eefd838cc14efa254140baf245d72e
mmalohlava <michal.malohlava@gmail.com>
2013-08-08 19:45:21 -0400

    Base skeleton.

---
+++ src/main/java/api/LChunk.java
@@ -0,0 +1,8 @@
+package api;
+
+/** 
+ * Low-level chunk using by KVstore to layout data.
+ */
+public interface LChunk {
+
+}
---
+++ src/main/java/api/View.java
@@ -0,0 +1,8 @@
+package api;
+
+/**
+ * Data view demanded by {@link Process} which imposes memory layout.
+ */
+public interface View {
+
+}
